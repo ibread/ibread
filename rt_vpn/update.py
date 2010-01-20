8d608529259f288aa3d5d44e7d9e39892313a8e7
breaddawson <breaddawson@e2ffff36-347d-11de-94d6-134fa40061eb>
2010-01-20 07:47:34 +0000

    Added CIDR_2_dot_decimal function, convert /8 to "255.0.0.0" Added support for openvpn

--- rt_vpn/update.py
+++ rt_vpn/update.py
@@ -3,7 +3,6 @@
 import os,sys
 
 def is_updated():
-
     # retrieve cn ips from apnic and preprocess
     status = os.system("curl -s http://ftp.apnic.net/apnic/dbase/data/country-ipv4.lst | grep cn | grep -oE [0-9.]+/[0-9]+ > cn_ipv4.lst");
     if status != 0:
@@ -17,7 +16,27 @@ def is_updated():
     else:
         return 1
 
-def update_ip_seg():
+def CIDR_2_dot_decimal(len):
+    '''
+    @desciption: given bit num of routing prefix, 
+                 such as "8" in "1.2.3.4/8",
+                 return its netmask as "255.0.0.0"
+    @param:
+        len - length of routing prefix, which is the number after "/"
+    @return:
+        netmask as a string
+    '''
+    result = ""
+    for i in range(4):
+        k = (len>8) and 8 or len
+        result = result+str(((2**k)-1)<<(8-k))+"."
+        if len > 8:
+            len -= 8
+        else:
+            len = 0
+    return result[:-1]
+
+def update_ip_seg(CIDR=0):
     ip_seg= open("cn_ipv4.lst","r")
     mac_ip_seg = open("mac_cn_ip.lst", "w+")
 
@@ -26,8 +45,15 @@ def update_ip_seg():
         result = line[:line.find('/')]
         for i in range(zeroes):
             result += ".0" 
-        result += line[line.find('/'):]
-        mac_ip_seg.write(result)
+        # which format?
+        # 1.2.3.4/8 or 1.2.3.4 255.0.0.0 ?
+        if CIDR:
+            result += line[line.find('/'):]
+            mac_ip_seg.write(result)
+        else:
+            result = result + " " + CIDR_2_dot_decimal(int(line[line.find('/')+1:].strip())) 
+            openvpn_wrap = "push \"route %s net_gateway 5\"\n\n"
+            mac_ip_seg.write(openvpn_wrap % (result))
 
 if __name__ == "__main__":
     print "* Checking ip database..."
