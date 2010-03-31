04a89e46e397f8114dc24510c532f609fad4695b
breaddawson <breaddawson@e2ffff36-347d-11de-94d6-134fa40061eb>
2010-03-31 13:49:49 +0000

    Changed update check mechanism from md5sum to HTTP Header Last-Modified

--- trunk/rt_vpn/update.py
+++ trunk/rt_vpn/update.py
@@ -5,25 +5,43 @@
 # @usage: Try ./update.py -h
 # @brief: This script is used to check retrieve the latest chinese ip database
 #       Here're several features:        
-#       1. Check if update is available (via md5_sum) 
+#       1. Check if update is available (via Last-Modified HTTP Header) 
 #       2. 2 output formats for net address: 
 #           -c: CIDR format, 1.2.0.0/16
 #           -d: Dot Deciaml format, 1.2.0.0 255.255.0.0
 #       3. cutomized output formats, such as the following format used for openvpn:
 #               push "route 1.2.0.0 255.255.0.0 net_gateway 5"
+# @ChangeLog:
+#    March 31, 2010:
+#       Change update check mechanism from md5sum to HTTP Header Last-Modified, Thanks to Paveo
 
 import os,sys
 from optparse import OptionParser
 
 def is_updated(out_file):
-    # retrieve cn ips from apnic and preprocess
-    status = os.system("curl -s http://ftp.apnic.net/apnic/dbase/data/country-ipv4.lst | grep cn | grep -oE [0-9.]+/[0-9]+ > pre_cn_db.lst");
+    '''
+        Check if there's update available for ip database.
+        If so, generate database for Chinese IP
+    
+        @return
+            1 if update is available, 0 if not or there's error
+    '''
+
+    # fetch Last-Modified info to determine if there's update available 
+    status = os.system("curl -Is http://ftp.apnic.net/apnic/dbase/data/country-ipv4.lst | grep 'Last-Modified' > new_update.txt")
     if status != 0:
         print "Error when downloading ip database or it's in invalid formate!"
         sys.exit(1)
 
-    # check if update is available or not
-    status = os.system("md5 pre_cn_db.lst %s &> new_md5_sig && diff new_md5_sig md5_sig &> /dev/null" % (out_file) )
+    status = os.system("diff new_update.txt last_modified.txt &> /dev/null")
+    
+    if status: 
+        # retrieve cn ips from apnic and preprocess
+        t_status = os.system("curl -s http://ftp.apnic.net/apnic/dbase/data/country-ipv4.lst | grep cn | grep -oE [0-9.]+/[0-9]+ > pre_cn_db.lst");
+        if t_status != 0:
+            print "Error when downloading ip database or it's in invalid formate!"
+            sys.exit(1)
+
     return status
 
 def CIDR_2_dot_decimal(origin):
@@ -54,6 +72,9 @@ def CIDR_2_dot_decimal(origin):
     return "%s %s" % (origin[:slash_pos], netmask[:-1])
 
 def update_ip_seg(out_file, CIDR=0, format="%s"):
+    '''
+        Process cn ip database and put them into out_file with CIDR/dot_decimal format  
+    '''
     fin = open("pre_cn_db.lst","r")
     fout = open(out_file, "w+")
 
@@ -114,11 +135,11 @@ if __name__ == "__main__":
         print "* Update available: ip database from apnic"
         print "* Preprocessing..."
         update_ip_seg(out_file, CIDR, out_format)
-        print "* Updating md5 signature..."
-        os.system("md5 pre_cn_db.lst cn_net_addr.lst > md5_sig")
+        print "* Updating last-modifed time" 
+        os.system("mv new_update.txt last_modified.txt")
         print "* Done. New cn_net_addr.lst is ready for route tables"
     else:
         print "* Nothing new, cn_net_addr.lst remains untouched." 
     
     # clear temp files
-    os.system("rm pre_cn_db.lst new_md5_sig")
+#    os.system("rm pre_cn_db.lst new_update.txt")
