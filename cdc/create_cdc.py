5f20ef6ace231a1a6d468c9cd2780adc636edad2
breaddawson <breaddawson@e2ffff36-347d-11de-94d6-134fa40061eb>
2010-12-04 07:35:31 +0000

    Complete

--- cdc/create_cdc.py
+++ cdc/create_cdc.py
@@ -45,7 +45,7 @@ def main():
     # primary output
     po = []
     # wires
-    wires = []
+    wire = []
     # dffs
     dffs = []
     # all statements
@@ -54,15 +54,15 @@ def main():
     for line in fin:
         if line.count("module s27") > 0:
             indicator = True
-
-        if indicator and line.count("endmodule") > 0:
-            indicator = False
     
         # Do nothing if we're outside of module or encountering comments
         if (not indicator) or line.strip().startswith("//"):
             fout.write(line)
             continue
-    
+
+        if indicator and line.count("endmodule") > 0:
+            indicator = False
+            
         statement += line.strip()
     
         # if no ";", this statement is not completed
@@ -80,30 +80,14 @@ def main():
             pi.extend(elements[1].strip(';').split(',')) 
         elif elements[0] == "output":
             po.extend(elements[1].strip(';').split(','))
-        elif elements[0] == "wires":
-            wires.extend(elements[1].strip(';').split(','))
+        elif elements[0] == "wire":
+            wire.extend(elements[1].strip(';').split(','))
         elif elements[0] == "dff":
-            dffs.extend(elements[1]) # DFF_0(CK,G5,G10);
+            dffs.append(elements[1]) # DFF_0(CK,G5,G10);
         else:
             all_statements.append(statement)
 
-        # if the statements startswith "input", "output", "wire"
-        # we need to output them immediately
-        if elements[0] in ["input", "output", "wire"]:
-            
-            fout.write("%s %s\n" % (elements[0], gen_pins(elements[1], "send")))
-            fout.write("%s %s\n" % (elements[0], gen_pins(elements[1], "recv")))            
-        else:
-            # dff DFF_0(CK,G5,G10);
-            module = elements[0] #dff 
-            (instance, pins) = elements[1].split('(') #DFF_0
-            pins = pins.rstrip(');') # CK,G5,G10
-            
-            if elements[0] == "module": # module s27
-                fout.write("%s %s(%s,%s);\n" % (module, instance, gen_pins(pins, "send"), gen_pins(pins, "recv")))
-            else:
-                fout.write("%s SEND_%s(%s);\n" % (module, instance, gen_pins(pins, "send")))
-                fout.write("%s RECV_%s(%s);\n" % (module, instance, gen_pins(pins, "recv")))
+
             
         # initialize statement for next round 
         statement = ""
@@ -113,23 +97,78 @@ def main():
     # output: o1 => dff(CK, oo1, o1)		
     for i in xrange(len(pi)):
         # these are not primary inputs
-        if i in ["CK", "GNN", "VDD"]:
+        if pi[i] in ["CK", "GND", "VDD"]:
             continue
-        dffs.extend("I_DFF(CK, %s, %s)" % (pi[i], "G"+pi[i]))
+        dffs.append("I_DFF(CK, %s, %s)" % (pi[i], "H"+pi[i]))
         pi[i] = "G" + pi[i]
         
     for i in xrange(len(po)):
-        dffs.extend("O_DFF(CK, %s, %s)" % ("G"+pi[i], pi[i]))
+        dffs.append("O_DFF(CK, %s, %s)" % ("H"+pi[i], pi[i]))
         po[i] = "G" + po[i]
             
     # output module
     # module s27 (CK, GND, ...)
-    fout.write("module %s(%s, %s);\n" % (instance, gen_pins(pi, "send"), gen_pins(pins, "recv")))
+    fout.write("module %s(%s, %s, %s, %s);\n\n" % (module, gen_pins(pi, "send"),  
+        gen_pins(po, "send"), gen_pins(pi, "recv"), gen_pins(po, "recv")))
     
-    #output input
+    #output input, wire, output
+    name = ["input", "wire", "output"]
+    vars = [pi, wire, po]
+    
+    for i in xrange(len(name)):
+        fout.write("%s %s, %s;\n" % (name[i], gen_pins(vars[i], "send"), gen_pins(vars[i], "recv")))
+    
+    fout.write("input scan_input;")
+    fout.write("input scan_enable;")
+    
+
+    # output DFF and connect the scan chain
+    fout.write("// scan chain begins here\n\n")
+    last_input = "scan_input"
+    
+    for d in dffs:
+        (instance, pins) = d.split('(')
+        pins = map(lambda x: x.strip(), pins.rstrip(');').split(','))
+        fout.write("SDFFNSR %s (.CK(%s), .D(%s), .Q(%s), .SI(%s), .SE(%s));\n" %  
+                            (instance, "send_"+pins[0], "send_"+pins[2], "send_"+pins[1], 
+                            last_input, "scan_enable"))
+        last_input = pins[1]
+    
+    for d in dffs:
+        (instance, pins) = d.split('(')
+        pins = map(lambda x: x.strip(), pins.rstrip(');').split(','))
+        fout.write("SDFFNSR %s (.CK(%s), .D(%s), .Q(%s), .SI(%s), .SE(%s));\n" %  
+                            (instance, "recv_"+pins[0], "recv_"+pins[2], "recv_"+pins[1], 
+                            last_input, "scan_enable"))
         
+    fout.write("// scan chain ends here\n\n")
+    
+    
+    # output all other statement
+    for state in all_statements:
+        # dff DFF_0(CK,G5,G10);
+        elements = state.split(' ', 1)
+        module = elements[0] #dff 
+        (instance, pins) = elements[1].split('(') #DFF_0
+        print instance, pins
+        pins = map(lambda x:x.strip(), pins.rstrip(');').split(',')) # CK,G5,G10
+        
+        fout.write("%s SEND_%s(%s);\n" % (module, instance, gen_pins(pins, "send")))
+        fout.write("%s RECV_%s(%s);\n" % (module, instance, gen_pins(pins, "recv")))        
+    
+
+    fout.write("endmodule\n")
         
 
 
 if __name__ == '__main__':
     main()
+
+#
+#        # if the statements startswith "input", "output", "wire"
+#        # we need to output them immediately
+#        if elements[0] in ["input", "output", "wire"]:
+#            
+#            fout.write("%s %s\n" % (elements[0], gen_pins(elements[1], "send")))
+#            fout.write("%s %s\n" % (elements[0], gen_pins(elements[1], "recv")))            
+#        else:
