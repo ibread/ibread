8f2a1a95d30d9584bde006786ca3be14f344735b
breaddawson <breaddawson@e2ffff36-347d-11de-94d6-134fa40061eb>
2010-12-05 02:54:22 +0000

    Connected outputs of Sender domain to inputs of Receiver Domain

--- cdc/create_cdc.py
+++ cdc/create_cdc.py
@@ -46,8 +46,8 @@ def main():
     po = []
     # wires
     wire = []
-    # dffs
-    dffs = []
+	# dffs
+	dffs = []
     # all statements
     all_statements = []
     
@@ -82,67 +82,111 @@ def main():
             po.extend(elements[1].strip(';').split(','))
         elif elements[0] == "wire":
             wire.extend(elements[1].strip(';').split(','))
-        elif elements[0] == "dff":
-            dffs.append(elements[1]) # DFF_0(CK,G5,G10);
+		elif elements[0] == "dff":
+			dffs.extend(elements[1].strip(';').split(','))
         else:
             all_statements.append(statement)
-
-
-            
+			
         # initialize statement for next round 
         statement = ""
 
-    # add dffs at the start & the end of the circuits
+	# remove some useless inputs
+	for s in ["CK", "GND", "VDD"]:
+		if s in pi:
+			pi.remove(s)
+	
+	# add dffs at the start & the end of the circuits
     # input: i1 => dff(CK, i1, ii1)
-    # output: o1 => dff(CK, oo1, o1)		
+    # output: o1 => dff(CK, oo1, o1)
+	#
+	# Note that since we merge two copies of the same benchmark together
+	# We could not just insert DFFs at the head and rear, and then double them
+	# DFFs at the rear of Sender domain and the head of the Receiver Domain
+	# needs to be connected together
+	#
+	# All added DFFs are at mid_dffs
+    mid_dffs = []
+	si = len(pi) - 3 # GND, VDD, CK are not primary inputs
+	so = len(po)
+	added_pi = []
+	added_po = []
+	
     for i in xrange(len(pi)):
         # these are not primary inputs
         if pi[i] in ["CK", "GND", "VDD"]:
             continue
-        dffs.append("I_DFF%d(CK, %s, %s)" % (i, pi[i], "H"+pi[i]))
-        pi[i] = "G" + pi[i]
+        mid_dffs.append("SEND_I_DFF%d(SEND_CK, SEND_%s, SEND_%s)" % (i, pi[i], "H"+pi[i]))
+        mid_dffs.append("RECV_I_DFF%d(RECV_CK, RECV_%s, RECV_%s)" % (i, pi[i], "H"+pi[i]))
+		added_pi.append("SEND_%s" % pi[i])
         
     for i in xrange(len(po)):
-        dffs.append("O_DFF%d(CK, %s, %s)" % (i, "H"+pi[i], pi[i]))
-        po[i] = "G" + po[i]
-            
+        mid_dffs.append("RECV_O_DFF%d(RECV_CK, RECV_%s, RECV_%s)" % (i, "H"+po[i], po[i]))
+        added_po.append("RECV_%s" % po[i])
+
+	# more inputs than outputs, which means inputs left to be primary inputs
+	if si >= so:
+		for i in xrange(si):
+			if i < so:
+				mid_dffs.append("MID_DFF%d(SEND_CK, RECV_%s, SEND_%s)" % (i, "H"+pi[i], po[i]))
+			else:
+				added_pi.append("RECV_%s" % pi[i])
+	else: # si < so
+		for i in xrange(so):
+			if i < si:
+				mid_dffs.append("MID_DFF%d(SEND_CK, RECV_%s, SEND_%s)" % (i, "H"+pi[i], po[i]))
+			else:
+				added_po.append("SEND_%s" % po[i])
+
     # output module
     # module s27 (CK, GND, ...)
-    fout.write("module %s(%s, %s, %s, %s);\n\n" % (module, gen_pins(pi, "send"),  
-        gen_pins(po, "send"), gen_pins(pi, "recv"), gen_pins(po, "recv")))
-    
+	#
+	# inputs include 3 parts: send_ + pi, recv_ + pi, added_pi
+	# so do outputs
+	pi.extend("CK", "GND", "VDD")
+	inputs = gen_pins(pi, "send") + gen_pins(pi, "recv") + ', '.join(added_pi)
+	outputs = gen_pins(po, "send") + gen_pins(po, "recv") + ', '.join(added_po)
+
+	fout.write("module %s (%s, %s, %s);\n\n" % (module, inputs, outputs))
+
     #output input, wire, output
-    name = ["input", "wire", "output"]
-    vars = [pi, wire, po]
-    
-    for i in xrange(len(name)):
-        fout.write("%s %s, %s;\n" % (name[i], gen_pins(vars[i], "send"), gen_pins(vars[i], "recv")))
+	fout.write("input %s;\n\n" % inputs)
+	fout.write("wire %s;\n\n" % gen_pins(wire, "send") + gen_pins(wire, "recv"))
+	fout.write("output %s;\n\n" % outputs)
     
     fout.write("input scan_input;")
     fout.write("input scan_enable;")
-    
 
     # output DFF and connect the scan chain
     fout.write("// scan chain begins here\n\n")
-    last_input = "scan_input"
+
+	last_input = "scan_input"
+	for md in mid_dffs:
+		(instance, pins) = md.split('(')
+		pins = map(lambda x: x.strip(), pins.rstrip(');').split(','))
+		
+		fout.write("SDFFNSR %s(.CK(%s), .D(%s), .Q(%s), .SI(%s), .SE(scan_enable));\n" %
+				   instance, pins[0], pins[2], pins[1], last_input)
+		last_input = pins[1]
     
-    for d in dffs:
-        (instance, pins) = d.split('(')
-        pins = map(lambda x: x.strip(), pins.rstrip(');').split(','))
-        fout.write("SDFFNSR SEND_%s (.CK(%s), .D(%s), .Q(%s), .SI(%s), .SE(%s));\n" %  
-                            (instance, "send_"+pins[0], "send_"+pins[2], "send_"+pins[1], 
-                            last_input, "scan_enable"))
-        last_input = pins[1]
-    
-    for d in dffs:
-        (instance, pins) = d.split('(')
-        pins = map(lambda x: x.strip(), pins.rstrip(');').split(','))
-        fout.write("SDFFNSR RECV_%s (.CK(%s), .D(%s), .Q(%s), .SI(%s), .SE(%s));\n" %  
-                            (instance, "recv_"+pins[0], "recv_"+pins[2], "recv_"+pins[1], 
-                            last_input, "scan_enable"))
-        
+	fout.write("// All orignal DFFs are extended into 2 copies")
+	for i in xrange(2):
+		if i == 0:
+			t = "send_"
+		else:
+			t = "recv_"
+		
+		for d in dffs:
+			(instance, pins) = d.split('(')
+			pins = map(lambda x: x.strip(), pins.rstrip(');').split(','))
+			fout.write("SDFFNSR %s%s (.CK(%s), .D(%s), .Q(%s), .SI(%s), .SE(%s));\n" %
+				                (t.swapcase(), instance, t+pins[0], t+pins[2], t+pins[1],
+								 last_input, "scan_enable"))
+			
+			last_input = t + pins[1]
+	fout.write("//END: All orignal DFFs are extended into 2 copies")
+
+	
     fout.write("// scan chain ends here\n\n")
-    
     
     # output all other statement
     for state in all_statements:
@@ -155,16 +199,13 @@ def main():
         
         fout.write("%s SEND_%s(%s);\n" % (module, instance, gen_pins(pins, "send")))
         fout.write("%s RECV_%s(%s);\n" % (module, instance, gen_pins(pins, "recv")))        
-    
 
     fout.write("endmodule\n")
-        
-
+    
 
 if __name__ == '__main__':
     main()
 
-#
 #        # if the statements startswith "input", "output", "wire"
 #        # we need to output them immediately
 #        if elements[0] in ["input", "output", "wire"]:
