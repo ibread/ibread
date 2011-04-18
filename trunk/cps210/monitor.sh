be7f5ef753b83dd8d5042d2fcab03a8558bf3899
breaddawson <breaddawson@e2ffff36-347d-11de-94d6-134fa40061eb>
2011-04-18 06:45:27 +0000

    Added iostat infomation

--- trunk/cps210/monitor.sh
+++ trunk/cps210/monitor.sh
@@ -7,3 +7,5 @@ echo -e "\n=========$(date)========" >> ${BASE}/ps.log
 ps -eo pcpu,size,vsize,cutime,cstime,utime,pid,user,args | sort -k 1 -r >> ${BASE}/ps.log
 echo -e "\n=========$(date)========" >> ${BASE}/raw_ps.log
 ps aux >> ${BASE}/raw_ps.log
+echo -e "\n=========$(date)========" >> ${BASE}/iostat.log
+iostat >> ${BASE}/iostat.log
